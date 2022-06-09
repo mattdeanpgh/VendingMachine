7b2d4acda0fb2b484d1f2a3398c1397406f371d7
Caitlin Nagelson <cnagelson@gmail.com>
2022-06-09 19:46:54 -0400

    Captstone 1 7:46

--- src/main/java/com/techelevator/Money.java
+++ src/main/java/com/techelevator/Money.java
@@ -2,53 +2,48 @@ package com.techelevator;
 
 public class Money {
     private double currentMoney = 0;
-    private double changeInCoins;
     private double productPrice;
-//    private static final double QUARTERS;
-//    private double NICKELS;
-//    private double DIMES;
 
-    public Money(double currentMoney, double changeInCoins, double productPrice) {
+    public Money(double currentMoney, double productPrice) {
         this.currentMoney = currentMoney;
-        this.changeInCoins = changeInCoins;
+
         this.productPrice = productPrice;
     }
 
+
+
+    public double getCurrentMoney() {
+        return currentMoney;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
     public double feedMoney(double moneyInserted) {
         currentMoney = currentMoney + moneyInserted;
         return currentMoney;
     }
 
     public void makePurchaseReturnChange() { //toString
-        // get the change due, find how many quarters/dimes/nickels to return
-        // change due / .
+        double change = currentMoney - productPrice;
         int quarterCounter = 0;
         int dimeCounter = 0;
         int nickelCounter = 0;
-        double change = currentMoney - productPrice; //make a method for making change and plug into here
         int quartersFromTruncation = (int) (change / .25);
-        int dimesFromTruncation = (int) ((change / .25) / .1);
-//        int nickelsFromTruncation = 0;
+        int dimesFromTruncation = (int) (((change * 100) - (quartersFromTruncation * 25)) / 10);
 
         if (change >= .25) {
             quarterCounter += change / .25;
-
         }
         if (change >=.10) {
-
-            change = (change * 100) - (quartersFromTruncation * 25);
-            dimeCounter += change /10;
-
+            change = change - ((quartersFromTruncation * 25) / 100);
+            dimeCounter += change /.1;
         }
         if (change >= .05) {
-            change = (change / .25);
+            change = change - ((((quartersFromTruncation * 25) + (dimesFromTruncation * 10)) / 100));
             nickelCounter += change / .05;
-
         }
         System.out.println("Change due is " + change + "(" + quarterCounter + " quarters, " + dimeCounter + " dimes, " + nickelCounter + " nickels)");
-
-
-
     }
 }
 
