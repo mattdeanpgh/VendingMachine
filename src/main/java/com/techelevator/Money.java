36f9693c294bff6bd78d00b7469be6fdf91c6f36
Caitlin Nagelson <cnagelson@gmail.com>
2022-06-09 16:25:07 -0400

    Capstone 1 4:24

--- src/main/java/com/techelevator/Money.java
+++ src/main/java/com/techelevator/Money.java
@@ -1,42 +1,56 @@
 package com.techelevator;
 
 public class Money {
-    private double currentMoney;
+    private double currentMoney = 0;
     private double changeInCoins;
+    private double productPrice;
 //    private static final double QUARTERS;
 //    private double NICKELS;
 //    private double DIMES;
 
-    public Money(double currentMoney, double changeInCoins) {
+    public Money(double currentMoney, double changeInCoins, double productPrice) {
         this.currentMoney = currentMoney;
         this.changeInCoins = changeInCoins;
+        this.productPrice = productPrice;
     }
 
-    public double feedMoney (double moneyInserted) {
+    public double feedMoney(double moneyInserted) {
         currentMoney = currentMoney + moneyInserted;
         return currentMoney;
     }
-//    public double makePurchase (double productPrice) {
-//        double change = 0;
-//          double changeInCoins = 0;
-//        change = (currentMoney - productPrice);
-//        if (change % QUARTERS == 0) {
-//            return changeInCoins;
-//        } else if ((change / QUARTERS) % DIMES == 0);
-//            return changeInCoins;
-//        } else if (((change / QUARTERS) / DIMES) % NICKELS == 0)
-//        return changeInCoins;
-//    }
-//    make a counter of each coin
+
+    public void makePurchaseReturnChange() { //toString
+        // get the change due, find how many quarters/dimes/nickels to return
+        // change due / .
+        int quarterCounter = 0;
+        int dimeCounter = 0;
+        int nickelCounter = 0;
+        double change = currentMoney - productPrice; //make a method for making change and plug into here
+        int quartersFromTruncation = (int) (change / .25);
+        int dimesFromTruncation = (int) ((change / .25) / .1);
+//        int nickelsFromTruncation = 0;
+
+        if (change >= .25) {
+            quarterCounter += change / .25;
+
+        }
+        if (change >=.10) {
+
+            change = (change * 100) - (quartersFromTruncation * 25);
+            dimeCounter += change /10;
+
+        }
+        if (change >= .05) {
+            change = (change / .25);
+            nickelCounter += change / .05;
+
+        }
+        System.out.println("Change due is " + change + "(" + quarterCounter + " quarters, " + dimeCounter + " dimes, " + nickelCounter + " nickels)");
 
 
-    public double changeReturned ()
 
-    /*
-    change return method:
-    current money - product price = change
-    changeToReturn = ((change % QUARTERS) % DIMES) % NICKELS
-    return = changeToReturn
-
-     */
+    }
 }
+
+//inventory reduction: inventory count needs to go down by 1 each time that item is purchased
+//if item inventory == 0 sout "SOLD OUT" and return to previous menu
