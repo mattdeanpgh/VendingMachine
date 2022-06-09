5ba4550a5571d6f3b8b3b1b1236feb49c655635f
Walt Impellicceiri <walt@techelevator.com>
2022-06-09 08:25:24 -0400

    Capstone starter code.

---
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -0,0 +1,32 @@
+package com.techelevator;
+
+import com.techelevator.view.Menu;
+
+public class VendingMachineCLI {
+
+	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
+	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
+	private static final String MAIN_MENU_OPTION_EXIT = "Exit";
+	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE, MAIN_MENU_OPTION_EXIT };
+
+	private final Menu menu;
+
+	public VendingMachineCLI(Menu menu) {
+		this.menu = menu;
+	}
+
+	public void run() {
+		while (true) {
+			String choice = (String) menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
+
+			switch (choice) {
+				case MAIN_MENU_OPTION_DISPLAY_ITEMS:
+					break;
+				case MAIN_MENU_OPTION_PURCHASE:
+					break;
+				case MAIN_MENU_OPTION_EXIT:
+					System.exit(0);
+			}
+		}
+	}
+}
