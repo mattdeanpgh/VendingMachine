e29d48280bf436737e6ab58c933616766a62fcfa
Caitlin Nagelson <cnagelson@gmail.com>
2022-06-10 10:13:16 -0400

    Capstone 1 10:13

--- src/main/java/com/techelevator/Money.java
+++ src/main/java/com/techelevator/Money.java
@@ -1,31 +1,47 @@
 package com.techelevator;
 
-public class Money {
-    private double currentMoney = 0;
-    private double productPrice;
+import java.io.*;
+import java.util.*;
 
-    public Money(double currentMoney, double productPrice) {
+public class Money extends Machine{
+    private double currentMoney = 0;
+    private double change;
+
+    public Money(double currentMoney, double change) {
         this.currentMoney = currentMoney;
 
-        this.productPrice = productPrice;
+        this.change = change;
     }
 
-
+    public void setCurrentMoney(double currentMoney) {
+        this.currentMoney = currentMoney;
+    }
 
     public double getCurrentMoney() {
         return currentMoney;
     }
+    public double getChange() throws FileNotFoundException {
+        File inventory = new File("vendingmachine.csv");
+        try (Scanner fileInput = new Scanner(inventory)) {
+            while (fileInput.hasNextLine()) {
+                String lineOfText = fileInput.nextLine();
+                {
+                    String[] productDetails = lineOfText.split("\\|");
+                    double productPrice = Double.parseDouble(productDetails[2]);
 
-    public double getProductPrice() {
-        return productPrice;
+                    change = currentMoney - productPrice;
+                }
+            }
+        }
+        return change;
     }
+
     public double feedMoney(double moneyInserted) {
         currentMoney = currentMoney + moneyInserted;
         return currentMoney;
     }
 
     public void makePurchaseReturnChange() { //toString
-        double change = currentMoney - productPrice;
         int quarterCounter = 0;
         int dimeCounter = 0;
         int nickelCounter = 0;
